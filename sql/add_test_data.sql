9a9bcebcdfa62a61084975a7efce5f0114eb0af0
Suonperä Ensio E M <ensu@da3-cs-dk110-03.pc.helsinki.fi>
2017-08-03 18:38:02 +0300

    Lisäsin sql-kyselyt taulujen poistamiseksi, luomiseksi ja testidatan lisäämiseksi.

--- sql/add_test_data.sql
+++ sql/add_test_data.sql
@@ -1,1 +1,41 @@
 -- Lisää INSERT INTO lauseet tähän tiedostoon
+-- Lisätään tuoteluokat 
+INSERT INTO Tuoteluokka (nimi) VALUES ('Asunnot');
+INSERT INTO Tuoteluokka (nimi) VALUES ('Pokemonit');
+
+
+-- Lisätään tuotteita
+INSERT INTO Tuote (nimi, kuvaus, kauppa_alkaa, kauppa_loppuu, minimihinta, linkki_kuvaan) 
+VALUES ('Kesämökki', 'Hyvien kulkuyhteyksien varrella.','2004-10-19 10:23:54+02', '2004-10-30 10:23:54+02', 30, 'https://upload.wikimedia.org/wikipedia/commons/e/e4/White_House_02.jpg');
+INSERT INTO Tuote (nimi, kuvaus, minimihinta, linkki_kuvaan)
+VALUES ('Moderni asunto', 'Nimi kertoo kaiken.', '100', 'https://upload.wikimedia.org/wikipedia/commons/a/af/All_Gizah_Pyramids.jpg');
+
+INSERT INTO Tuote (nimi, kuvaus, kauppa_alkaa, kauppa_loppuu, minimihinta, linkki_kuvaan)
+VALUES ('Psykoankka', 'Osta, jos uskallat.', '2017-07-17 17:05:47', '2017-10-10 17:00:00', 100, 'https://pixabay.com/p-1629601/?no_redirect' );
+
+
+-- Lisätään tuotteet luokkiin
+INSERT INTO Luokan_tuote (tuote, tuoteluokka) VALUES (1,1);
+INSERT INTO Luokan_tuote (tuote, tuoteluokka) VALUES (2,1);
+INSERT INTO Luokan_tuote (tuote, tuoteluokka) VALUES (3,2);
+
+-- Lisätään henkilötiedot
+INSERT INTO Henkilotiedot (nimi, sahkoposti, osoite)
+VALUES ('Ensio', 'ensio.suonpera@helsinki.fi', 'Höpötie 31');
+INSERT INTO Henkilotiedot (nimi, sahkoposti, osoite)
+VALUES ('TESTAAJA', 'testi@testiposti.fi', 'Mannerhemintie 5, Helsiki');
+INSERT INTO Henkilotiedot (nimi, sahkoposti, osoite)
+VALUES ('TESTAAJATAR', 'testitys@testiposti.fi', 'Mannerhemintie 5b, Helsiki');
+
+-- Lisätään asiakastilit
+INSERT INTO Asiakastili (henkilotiedot, kayttajatunnus, salasana)
+VALUES (1, 'ensu', 'Salasana1');
+INSERT INTO Asiakastili (henkilotiedot, kayttajatunnus, salasana)
+VALUES (3, 'NAINEN', 'Salasana3');
+INSERT INTO Asiakastili (henkilotiedot, kayttajatunnus, salasana, Meklari)
+VALUES (2, 'MIES', 'Salasana2', TRUE);
+
+-- Lisätään Tarjouksia
+INSERT INTO Tarjous (tuote, henkilotiedot, summa) VALUES (2, 3, 112);
+INSERT INTO Tarjous (tuote, henkilotiedot, summa) VALUES (2, 1, 144);
+INSERT INTO Tarjous (tuote, henkilotiedot, summa) VALUES (3, 3, 1000);
--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,1 +1,43 @@
--- Lisää CREATE TABLE lauseet tähän tiedostoon
+CREATE TABLE Tuoteluokka(
+    tuoteluokka_id SERIAL PRIMARY KEY,
+    nimi varchar(50)
+);
+
+CREATE TABLE Tuote(
+    tuote_id SERIAL PRIMARY KEY,
+    nimi varchar(100) NOT NULL,
+    kuvaus text NOT NULL,   
+    kauppa_alkaa timestamp,
+    kauppa_loppuu timestamp,
+    minimihinta INTEGER NOT NULL,
+    linkki_kuvaan varchar(200)
+);
+
+CREATE TABLE Luokan_tuote(
+    id SERIAL PRIMARY KEY,
+    tuote INTEGER REFERENCES Tuote(tuote_id) NOT NULL,
+    tuoteluokka INTEGER REFERENCES Tuoteluokka(tuoteluokka_id)        
+    NOT NULL    
+);
+
+CREATE TABLE Henkilotiedot(
+    henkilo_id SERIAL PRIMARY KEY,
+    nimi varchar(100) NOT NULL,
+    sahkoposti varchar(50) NOT NULL,
+    osoite varchar(150) NOT NULL
+);
+
+CREATE TABLE Tarjous(
+    tarjous_id SERIAL PRIMARY KEY,
+    tuote INTEGER REFERENCES Tuote(tuote_id) NOT NULL,
+    henkilotiedot INTEGER REFERENCES Henkilotiedot(henkilo_id) NOT NULL,
+    summa INTEGER NOT NULL
+);
+
+CREATE TABLE Asiakastili(
+    asiakastili_id SERIAL PRIMARY KEY,
+    henkilotiedot INTEGER REFERENCES Henkilotiedot(henkilo_id) NOT NULL,
+    kayttajatunnus varchar(20) NOT NULL,
+    salasana varchar(100) NOT NULL,
+    Meklari boolean DEFAULT FALSE
+);
