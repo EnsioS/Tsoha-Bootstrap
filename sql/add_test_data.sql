bbab8e1a3236c5788575aa2ae37355831c5d18c0
Suonperä Ensio E M <ensu@da3-cs-dk110-13.pc.helsinki.fi>
2017-08-23 14:39:29 +0300

    lisäsin testidataa ja työskentilin sivuilla näytettävän ajan kanssa

--- sql/add_test_data.sql
+++ sql/add_test_data.sql
@@ -3,6 +3,8 @@
 INSERT INTO Tuoteluokka (nimi) VALUES ('Asunnot');
 INSERT INTO Tuoteluokka (nimi) VALUES ('Pokemonit');
 INSERT INTO Tuoteluokka (nimi) VALUES ('POISTETTAVA');
+INSERT INTO Tuoteluokka (nimi) VALUES ('Lemmikit');
+INSERT INTO Tuoteluokka (nimi) VALUES ('Taide');
 
 
 -- Lisätään tuotteita
@@ -17,17 +19,25 @@ VALUES ('Moderni asunto 3', 'Nimi kertoo kaiken.', '100', 'https://upload.wikime
 INSERT INTO Tuote (nimi, kuvaus, minimihinta, linkki_kuvaan)
 VALUES ('Pokemon center', '"Haluutko parantaa sun pokemonit?"', '10000', 'https://c1.staticflickr.com/5/4070/4549247371_3aecce8246_z.jpg');
 INSERT INTO Tuote (nimi, kuvaus, kauppa_alkaa, kauppa_loppuu, minimihinta, linkki_kuvaan)
-VALUES ('Machu Picchu', 'Osta, jos uskallat.', '2017-07-17 17:05:47', '2017-10-10 17:00:00', 100, 'http://orig07.deviantart.net/bd39/f/2010/074/e/7/pichu_animacion_by_dark_clefita.gif' );
-
+VALUES ('Machu Picchu', 'Osta, jos uskallat.', '2017-07-17 17:05:47', '2017-10-10 17:00:00', 100, 'http://orig07.deviantart.net/bd39/f/2010/074/e/7/pichu_animacion_by_dark_clefita.gif');
+INSERT INTO Tuote (nimi, kuvaus, kauppa_alkaa, kauppa_loppuu, minimihinta, linkki_kuvaan)
+VALUES ('Mursu', 'Lämmin ja halattava lemmikki.', '2017-07-17 00:00:00', '2017-10-10 17:00:00', 65, 'https://c1.staticflickr.com/5/4006/4684089330_4b6b70c413_b.jpg');
+INSERT INTO Tuote (nimi, kuvaus, kauppa_alkaa, kauppa_loppuu, minimihinta, linkki_kuvaan)
+VALUES ('Ihana kissa', 'Niin hellyttävä katse.', '2017-07-17 00:00:00', '2017-10-10 17:00:00', 150, 'https://mgl.skyrock.net/art/SHAR.1867.268.2.jpg');
+INSERT INTO Tuote (nimi, kuvaus, kauppa_alkaa, kauppa_loppuu, minimihinta, linkki_kuvaan)
+VALUES ('Kissanpentu', 'Niin hellyttävä katse.', '2017-11-17 00:00:00', '2017-12-10 17:00:00', 150, 'https://upload.wikimedia.org/wikipedia/commons/9/9e/Green_eyes_kitten.jpg'); 
 
 -- Lisätään tuotteet luokkiin
-INSERT INTO Luokan_tuote (tuote, tuoteluokka) VALUES (1,1);
-INSERT INTO Luokan_tuote (tuote, tuoteluokka) VALUES (2,1);
-INSERT INTO Luokan_tuote (tuote, tuoteluokka) VALUES (3,1);
-INSERT INTO Luokan_tuote (tuote, tuoteluokka) VALUES (4,1);
-INSERT INTO Luokan_tuote (tuote, tuoteluokka) VALUES (5,1);
-INSERT INTO Luokan_tuote (tuote, tuoteluokka) VALUES (5,2);
-INSERT INTO Luokan_tuote (tuote, tuoteluokka) VALUES (6,2);
+INSERT INTO Luokan_tuote (tuote, tuoteluokka) VALUES (1, 1);
+INSERT INTO Luokan_tuote (tuote, tuoteluokka) VALUES (2, 1);
+INSERT INTO Luokan_tuote (tuote, tuoteluokka) VALUES (3, 1);
+INSERT INTO Luokan_tuote (tuote, tuoteluokka) VALUES (4, 1);
+INSERT INTO Luokan_tuote (tuote, tuoteluokka) VALUES (5, 1);
+INSERT INTO Luokan_tuote (tuote, tuoteluokka) VALUES (5, 2);
+INSERT INTO Luokan_tuote (tuote, tuoteluokka) VALUES (6, 2);
+INSERT INTO Luokan_tuote (tuote, tuoteluokka) VALUES (7, 4);
+INSERT INTO Luokan_tuote (tuote, tuoteluokka) VALUES (8, 4);
+INSERT INTO Luokan_tuote (tuote, tuoteluokka) VALUES (9, 4);
 
 -- Lisätään henkilötiedot
 INSERT INTO Henkilotiedot (nimi, sahkoposti, osoite)
